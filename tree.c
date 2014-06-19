a0b30cf5969887ff34a6f41b2dc6fa617d800a1c
livinsunny <livinsunny@users.noreply.github.com>
2014-06-19 19:35:06 +0530

    adding more functions

--- tree.c
+++ tree.c
@@ -39,5 +39,46 @@ treenode_t* insert(treenode_t *node , int element){
 
 }
 
+	//deleting an element from the tree
+
+treenode_t *delete(treenode_t node,int data){
+
+	treenode_t *temp;
+	if (node == NULL)
+	{
+		return;
+	}
+	else if (data < node->data)
+	{
+		node->left =delete(node->left,data);
+	}
+	else if (data > node->data)
+	{
+		node->right =delete(node->right,data);
+	}
+	else
+	{
+		//Now we replace with minimum element in the right 
+		//sub tree or maximum element in the sub tree
+		if (node->right && node->left)
+		{
+			//here we will replace with minimum element in 
+			//the right sub tree
+		}
+
+
+
+
+
+		//If there is only one/ zero child then we can directly
+		//remove it from the tree and connect itd parents to 
+		//its child
+		temp = node;
+		if (node->left == NULL )
+		{	
+			
+		}
+	}
+}
 
 
