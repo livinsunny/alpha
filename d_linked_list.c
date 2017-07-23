1013bf62ba5d741f19968dca55763a8ef5e8bacb
Livin Sunny <livin20@gmail.com>
2017-07-23 20:46:28 +0530

    Added DLL functions to insert front and insert end.

--- d_linked_list.c
+++ d_linked_list.c
@@ -27,13 +27,82 @@
  */
 
 
- #include <stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
 
- struct node
+typedef struct node
  {
  	int data;
- 	node* next;
- 	node* prev;
+ 	struct node* next;
+ 	struct node* prev;
  	
  }node_t;
+
+void push(node_t **head, int newdata)
+{
+	node_t *newnode = (node_t*)malloc(sizeof(node_t));
+	newnode->data=newdata;
+	
+	newnode->next=*head;
+	newnode->prev=NULL;
+
+	if(*head != NULL)
+	{
+		(*head)->prev=newnode;
+	}
+	
+	*head=newnode;
+}
+
+void insert_end(node_t **head, int newdata)
+{
+	node_t *temp=*head;
+	node_t *newnode = (node_t*)malloc(sizeof(node_t));
+	newnode->data=newdata;
+	
+	newnode->next=NULL;
+
+	if(temp == NULL)
+	{
+		newnode->prev=NULL;
+		*head=newnode;
+		return;
+	}
+	
+	while(temp->next!=NULL)
+	{
+		temp=temp->next;
+	}
+	temp->next=newnode;
+	newnode->prev=temp;
+}
+
+void print(node_t **head)
+{
+	node_t *temp=*head;
+	printf("\n");
+	while(temp!=NULL)
+	{
+		printf("%d ->",temp->data);
+		temp=temp->next;
+	}
+}
+
+int main()
+{
+	node_t *head=NULL;
+	//Create a the DLL
+	push(&head,31);
+	push(&head,4);
+	push(&head,22);
+	push(&head,24);
+	push(&head,90);
+	push(&head,50);
+	push(&head,20);
+	print(&head);
+	insert_end(&head,99);
+	print(&head);	
+	//Release the memory at the end.
+	return 0;
+}
 
